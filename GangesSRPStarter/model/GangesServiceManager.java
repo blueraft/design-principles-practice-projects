b82584502dbf66c5badf324d8b1412a0d47bb2ba
Ahmed Nahzan <nahzaaan@gmail.com>
2017-12-12 12:51:32 +0100

    Added a Purchase class and refactored GangesServicesManager and DataStorage

--- GangesSRPStarter/model/GangesServiceManager.java
+++ GangesSRPStarter/model/GangesServiceManager.java
@@ -8,44 +8,26 @@ import java.util.List;
 /**
  * model.Customer interface for making requests of various Ganges.com, Inc. services.
  */
-public class GangesServiceManager extends DataStorage {
+public class GangesServiceManager {
 
-    protected HashMap<Integer, String> dataStorage;
-    protected List<Book> books;
-    protected List<BookOrder> customerBookOrders;
-    protected int orderNumberCounter;
-    protected int customerIdCounter;
+    private DataStorage dataStorage;
+    private Purchase bookPurchase;
 
-    public static final int FIRST_CUSTOMER_NUMBER = 100;
-    private static final int FIRST_ORDER_NUMBER = 1000;
 
     public GangesServiceManager() {
-        super();
-
-        this.books = new ArrayList<>();
-        books.add(new Book ("A Tale of Two Gentlemen of Verona", 5.99));
-        books.add(new Book("Harry Potter and a Series of Unfortunate Events", 12.49));
-        books.add(new Book("Fifty Shades of The Color Purple", 22.36));
-
-        this.customerBookOrders = new ArrayList<>();
-
-        this.customerIdCounter = FIRST_CUSTOMER_NUMBER;
-        this.orderNumberCounter = FIRST_ORDER_NUMBER;
+        this.dataStorage = new DataStorage();
+        this.bookPurchase = new Purchase();
     }
 
     // EFFECTS: Create a new customer with the given address and name
     public Customer signUpNewCustomer(String address, String name) {
-        return new Customer(name, address, customerIdCounter++);
+        return bookPurchase.signUpNewCustomer(address,name);
     }
 
 
     // EFFECTS: Print all books for sale to the console, along with the index number + 1.
     public void listAvailableBooks() {
-        System.out.println("Books available through Ganges:");
-        for (int i = 0; i < books.size(); i++) {
-            Book thisBook = books.get(i);
-            System.out.println((i+1) + ". " + thisBook.getTitle() + " - " + thisBook.getPrice());
-        }
+        bookPurchase.listAvailableBooks();
     }
 
     // REQUIRES: bookNumber is >= 1
@@ -53,39 +35,28 @@ public class GangesServiceManager extends DataStorage {
     // EFFECTS: If the book number corresponds to a book in stock (i.e., is a valid index+1 of the list books),
     //          then create a new model.BookOrder and return the order number; otherwise, do not create an order and return -1.
     public BookOrder orderBook(Customer c, int bookNumber) {
-        bookNumber--; // change book number to zero-point indexing
-
-        if (bookNumber < books.size()) {
-            Book thisBook = books.get(bookNumber);
-            int thisOrderNum = orderNumberCounter++;
-
-            BookOrder order = new BookOrder(c, thisBook, thisOrderNum);
-            customerBookOrders.add(order);
-
-            System.out.println("Your order for " + thisBook.getTitle() + " is on the way!");
-            return order;
-        }
-        else {
-            System.out.println("Sorry, that isn't a book we have in stock.");
-            return null;
-        }
+        return bookPurchase.orderBook(c,bookNumber);
     }
 
     // MODIFIES: this
     // EFFECTS: if this order number matches a current order, then the order is cancelled (removed from the orders list)
     public boolean cancelBookOrder(int orderNumber) {
-        Iterator<BookOrder> iterator = customerBookOrders.iterator();
-        BookOrder currOrder;
+        return bookPurchase.cancelBookOrder(orderNumber);
+    }
 
-        while (iterator.hasNext()) {
-            currOrder = iterator.next();
+    public void putData(Customer c, String data) {
+        dataStorage.putData(c, data);
+    }
 
-            if (currOrder.getOrderNumber() == orderNumber) {
-                iterator.remove();
-                return true;
-            }
-        }
-        return false;
+    // Returns the given customer's data from the cloud
+    public String getData(Customer c) {
+        return dataStorage.getData(c);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Deletes this customer's data from the cloud
+    public String deleteData(Customer c) {
+        return dataStorage.deleteData(c);
     }
 
 }
